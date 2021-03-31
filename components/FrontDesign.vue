8de129c5e41d41b0227162196baf4ac354a18f99
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2021-03-31 15:51:41 +0900

    add timer in front page

--- components/FrontDesign.vue
+++ components/FrontDesign.vue
@@ -1,17 +1,18 @@
+import { default } from '../pages/techblog/_title.vue';
 <template>
   <div class="text-green-900">
     <section class="container bg-gradient-to-r from-white to-gray-100 ">
       <div>
         <navigation />
 
-        <h1 class="text-6xl p-6">
+        <h1 class="text-6xl p-6 welcome">
           Hey, welcome here :)
         </h1>
-        <p class="text-3xl">
+        <p class="text-3xl font-medium font-sans">
           I'm Aza
         </p>
         <p class="text-4xl p-4">
-          Software Engineer
+          {{ showMajor }}
         </p>
       </div>
     </section>
@@ -44,13 +45,48 @@
     </section>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      major: ['Software engineer', 'Full stack developer', 'Front-end developer', 'Research & Dev', 'Amateur blogger'],
+      showMajor: '',
+      timer: null
+
+    }
+  },
+  mounted () {
+    this.updateMajor(this.major, this.delegate, 3000)
+  },
+
+  methods: {
+    updateMajor (arr, delegate, delay) {
+      let i = 0
+      const interval = setInterval(() => {
+        delegate(arr[i])
+        if (i++ >= arr.length - 1) {
+          clearInterval(interval)
+        }
+      }, delay)
+    },
+    delegate (value) {
+      this.showMajor = value
+    }
+  }
+}
+</script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500&family=Syne+Mono&display=swap');
 
 #bg-img{
   background-image: url('../assets/bg1.jpg');
 }
 #bg-img1{
   background-image: url('../assets/bg4.jpg');
+}
+
+.welcome{
+  font-family: 'Noto Sans TC', sans-serif;
 }
 </style>
