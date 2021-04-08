34f8904f965a5f53c4943f06fc12344fc6a95aae
Azjargal13 <azjargal.gankhuyag@konicaminolta.com>
2021-04-08 10:00:30 +0900

    update about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="container flex justify-center">
+  <div class="container flex justify-center content-start">
     <article class="prose lg:prose-xl">
       <h1>About me: Who is Aza? </h1>
       <p>
-        For years parents have espoused the health benefits of eating garlic bread with cheese to their
-        children, with the food earning such an iconic status in our culture that kids will often dress
-        up as warm, cheesy loaf for Halloween.
+        I'm somebody who likes to question things a lot and try to find answers by reading, observing, and listening.
       </p>
-      <p>
-        But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-        springing up around the country.
-      </p>
-       <p>
-        For years parents have espoused the health benefits of eating garlic bread with cheese to their
-        children, with the food earning such an iconic status in our culture that kids will often dress
-        up as warm, cheesy loaf for Halloween.
-      </p>
-      <p>
-        But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-        springing up around the country.
-      </p>
+      <h4>some Facts</h4>
+      <ul>
+        <li>
+          born and raised in Ulaanbaatar, Mongolia.
+        </li>
+        <li>
+          studied Bachelor in Computer Science, majored hardware & network in Ulaanbaatar, Mongolia.
+        </li>
+        <li>
+          studied Master in Computer Science, majored CSE in Mumbai, India.
+        </li>
+        <li>
+          work as a software engineer in Tokyo, Japan.
+        </li>
+      </ul>
 
+      <p>
+        you can find my detailed resume on LinkedIn, so please visit there.
+      </p>
     </article>
   </div>
 </template>
