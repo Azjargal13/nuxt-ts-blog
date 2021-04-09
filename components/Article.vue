95c6bc3f140d6fcb44cb0b6e9d897e296763bd0b
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-04-09 10:01:46 +0900

    blog and about page margin update

--- components/Article.vue
+++ components/Article.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="shadow-md">
-    <article v-for="post in article" :key="post.id" class="prose lg:prose-xl">
-      <h1>{{ post.title }}</h1>
-      <h5>#Label</h5>
-      <h5>#Tags</h5>
-      <div class="mt-2 mr-2 clock-icon text-lg ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4 clock mb-1"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        {{ formatTime(post.createdAt) }}
-      </div>
-      <nuxt-content :document="post" />
-    </article>
+  <div class="max-w-full container ">
+    <section class=" prose prose-sm sm:prose lg:prose-lg mx-auto md:m-2 sm:m-4">
+      <article v-for="post in article" :key="post.id" class="shadow-md rounded p-4 md:p-12 shadow-2xl lg:w-3/4">
+        <h1>
+          {{ post.title }}
+        </h1>
+        <h5>#Label</h5>
+        <h5>#Tags</h5>
+        <div class="mt-2 mr-2 clock-icon text-lg ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4 clock mb-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          {{ formatTime(post.createdAt) }}
+        </div>
+        <nuxt-content
+          :document="post"
+        />
+      </article>
+    </section>
   </div>
 </template>
 
@@ -56,5 +62,8 @@ export default {
 <style scoped>
 .clock{
   display: inline-block;
+}
+.container{
+  text-align: justify;
 }
 </style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="container flex justify-center content-start">
-    <article class="prose lg:prose-xl">
-      <h1>About me: Who is Aza? </h1>
-      <p>
-        I'm somebody who likes to question things a lot and try to find answers by reading, observing, and listening.
-      </p>
-      <h4>some Facts</h4>
-      <ul>
-        <li>
-          born and raised in Ulaanbaatar, Mongolia.
-        </li>
-        <li>
-          studied Bachelor in Computer Science, majored hardware & network in Ulaanbaatar, Mongolia.
-        </li>
-        <li>
-          studied Master in Computer Science, majored CSE in Mumbai, India.
-        </li>
-        <li>
-          work as a software engineer in Tokyo, Japan.
-        </li>
-      </ul>
+  <div class="container">
+    <section class="flex">
+      <article class="prose prose-sm lg:prose-xl sm:prose-sm">
+        <h1>About me: Who is Aza? </h1>
+        <p>
+          I'm somebody who likes to question things a lot and try to find answers by reading, observing, and listening.
+        </p>
+        <h4>some Facts</h4>
+        <ul>
+          <li>
+            born and raised in Ulaanbaatar, Mongolia.
+          </li>
+          <li>
+            studied Bachelor in Computer Science, majored hardware & network in Ulaanbaatar, Mongolia.
+          </li>
+          <li>
+            studied Master in Computer Science, majored CSE in Mumbai, India.
+          </li>
+          <li>
+            work as a software engineer in Tokyo, Japan.
+          </li>
+        </ul>
 
-      <p>
-        you can find my detailed resume on LinkedIn, so please visit there.
-      </p>
-    </article>
+        <p>
+          you can find my detailed resume on LinkedIn, so please visit there.
+        </p>
+      </article>
+    </section>
   </div>
 </template>
 
@@ -36,5 +38,7 @@ export default {
 </script>
 
 <style scoped>
-
+.container{
+  text-align: justify;
+}
 </style>
