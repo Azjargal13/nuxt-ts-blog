605dbe429fbe889551ee2c39a7d355bfa1043c0a
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-04-13 22:05:28 +0900

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    parser: "babel-eslint",
+    sourceType: "module"
   },
   extends: [
-    '@nuxtjs/eslint-config'
+    "@nuxtjs/eslint-config"
   ],
   rules: {
-    'consistent-return': 2,
+    "consistent-return": 2,
     indent: 2,
-    'no-else-return': 1,
-    semi: 1,
-    'space-unary-ops': 2,
-    'no- tabs': 0
-  }
+    "no-else-return": 1,
+    semi: "off",
+    "space-unary-ops": 2,
+    "no- tabs": 0,
 
-};
+    quotes: [2, "double", { avoidEscape: true }],
+    "space-before-function-paren": "always"
+  }
+}
