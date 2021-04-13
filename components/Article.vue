f4bad48fed0e07107e37702c1aa63b031e5bf8d8
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-04-13 22:57:53 +0900

    add tags, label option

--- components/Article.vue
+++ components/Article.vue
@@ -9,12 +9,29 @@
         <h1>
           {{ post.title }}
         </h1>
-        <h5>#Label</h5>
-        <h5>#Tags</h5>
-        <div class="mt-2 mr-2 clock-icon text-lg ">
+        <div class="mb-2 flex flex-row justify-center">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="h-4 w-4 mt-3 mr-1"
+          >
+            <path
+              fill="currentColor"
+              d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z"
+            ></path>
+          </svg>
+          <span v-for="item in post.tag" :key="item.id" class="m-0.5">
+            {{ item }} |
+          </span>
+        </div>
+
+        <div class="mt-2 mr-2 clock-icon text-lg flex justify-end ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4 clock mb-1"
+            class="h-4 w-4 clock mt-1.5 mr-0.5"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -27,13 +44,13 @@
             />
           </svg>
           {{ formatTime(post.createdAt) }}
-          <img
-            v-if="post.image"
-            :src="post.image"
-            alt="img"
-            class="max-h-1/5 min-w-sm"
-          />
         </div>
+        <img
+          v-if="post.image"
+          :src="post.image"
+          alt="img"
+          class="max-h-1/5 min-w-sm"
+        />
         <nuxt-content :document="post" />
       </article>
     </section>
--- components/BlogList.vue
+++ components/BlogList.vue
@@ -1,39 +1,62 @@
 <template>
-  <div class="container flex flex-col">
+  <div class="container flex flex-col divide-y-2 divide-green-500 ">
     <div v-for="post in articles" :key="post.id" class="blog-list m-6">
-      <div class="blog-col flex flex-row">
-        <div class="">
-          <NuxtLink :to="post.path" class="hover:text-green-500 ">
-            <ul class="lg:text-2xl font-medium sm:text-lg mb-2">
-              <li>{{ post.title }}</li>
-            </ul>
-            <!-- {{ post }} -->
+      <div class="blog-col ">
+        <NuxtLink :to="post.path" class="hover:text-green-500 ">
+          <ul class="lg:text-2xl font-medium sm:text-lg mb-2">
+            <li>{{ post.title }}</li>
+          </ul>
+        </NuxtLink>
+        <!-- {{ post }} -->
 
-            <div class="mb-2">
-              # tag or # label
-            </div>
-            <div class="mt-2 clock-icon">
-              <svg
+        <div class="mb-2 flex flex-row hover:text-green-500">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="h-4 w-4 mt-2 mr-1"
+          >
+            <path
+              fill="currentColor"
+              d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z"
+            ></path>
+          </svg>
+          <span v-for="item in post.tag" :key="item.id" class="m-0.5">
+            {{ item }} |
+          </span>
+
+          <!-- <svg
+                class="h-6 w-6"
+                aria-hidden="true"
+                focusable="false"
+                role="img"
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 clock"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                viewBox="0 0 448 512"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              {{ formatTime(post.createdAt) }}
-            </div>
-          </NuxtLink>
+                  fill="currentColor"
+                  d="M446.2 270.4c-6.2-19-26.9-29.1-46-22.9l-45.4 15.1-30.3-90 45.4-15.1c19.1-6.2 29.1-26.8 23-45.9-6.2-19-26.9-29.1-46-22.9l-45.4 15.1-15.7-47c-6.2-19-26.9-29.1-46-22.9-19.1 6.2-29.1 26.8-23 45.9l15.7 47-93.4 31.2-15.7-47c-6.2-19-26.9-29.1-46-22.9-19.1 6.2-29.1 26.8-23 45.9l15.7 47-45.3 15c-19.1 6.2-29.1 26.8-23 45.9 5 14.5 19.1 24 33.6 24.6 6.8 1 12-1.6 57.7-16.8l30.3 90L78 354.8c-19 6.2-29.1 26.9-23 45.9 5 14.5 19.1 24 33.6 24.6 6.8 1 12-1.6 57.7-16.8l15.7 47c5.9 16.9 24.7 29 46 22.9 19.1-6.2 29.1-26.8 23-45.9l-15.7-47 93.6-31.3 15.7 47c5.9 16.9 24.7 29 46 22.9 19.1-6.2 29.1-26.8 23-45.9l-15.7-47 45.4-15.1c19-6 29.1-26.7 22.9-45.7zm-254.1 47.2l-30.3-90.2 93.5-31.3 30.3 90.2-93.5 31.3z"
+                ></path>
+              </svg> -->
         </div>
-
-        <div v-if="post.image">
-          <img :src="post.image" alt="img" class="max-h-36 max-w-36 rounded p-2 ">
+        <div class="mt-4 clock-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4 clock mb-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          {{ formatTime(post.createdAt) }}
         </div>
       </div>
     </div>
@@ -41,7 +64,7 @@
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 export default {
   props: {
     path: {
@@ -49,30 +72,30 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {
       articles: []
-    }
+    };
   },
-  async mounted () {
-    this.articles = await this.$content(this.path).fetch()
+  async mounted() {
+    this.articles = await this.$content(this.path).fetch();
   },
   methods: {
-    formatTime (value) {
-      return moment(value).format('MMMM Do YYYY')
+    formatTime(value) {
+      return moment(value).format("MMMM Do YYYY");
     }
   }
-}
+};
 </script>
 
 <style scoped>
-.clock{
+.clock {
   display: inline-block;
 }
-.blog-list{
+.blog-list {
   text-align: justify;
 }
-.container{
+.container {
   justify-content: start;
   align-items: flex-start;
 }
