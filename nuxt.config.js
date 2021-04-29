2851287f21995a362ecef1da99313ea0d947dcc7
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-04-29 16:49:22 +0900

    update meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,32 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
   loading: {
-    color: 'green',
-    height: '5px'
+    color: "green",
+    height: "5px"
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Azaa | Blog',
+    title: "Aza | Blog :)",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "author", content: "Aza, Azaa, Azjargal, Azjargal Gankhuyag" },
+      { name: "keywords", content: "aza blog, azaa's blog, azaa's writing, tech-blog, aza tech blog" },
+      { hid: "description", name: "description", content: "Azaa's personal blog for sharing tech and non-tech short articles and thoughts, some knowledge sharing activity purpose" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/logo.png" }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/styles/style.css'
+    "~assets/styles/style.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -37,15 +39,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxt/content'
+    "@nuxt/content"
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
