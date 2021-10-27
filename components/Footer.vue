017ba33f7cb224752de95fe057e4af3f127d5441
Azjargal13 <azjargal.gankhuyag@virtusize.com>
2021-10-27 18:54:19 +0900

    footer style update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="flex justify-center h-12 static bottom-0">
-    <p class="prose lg:prose-lg">
-      &copy; Aza in {{ date }}
-    </p>
+    <div class="divider flex flex-row justify-center">
+      <p class="prose lg:prose-lg dark:text-white text-center">
+        &copy; Aza in {{ date }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       date: new Date().getFullYear()
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-
+.divider {
+  width: 80%;
+  margin-top: 10px;
+  border-top: 1px rgba(16, 185, 129) solid;
+}
 </style>
